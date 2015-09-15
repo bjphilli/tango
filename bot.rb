506e67cf0e0f4f7c0528a97cf9c98bba2ff2920f
Blake <phillips.blakej@gmail.com>
2015-09-14 20:25:57 -0700

    Initial commit

---
+++ bot.rb
@@ -0,0 +1,223 @@
+require 'cinch'
+require 'sqlite3'
+
+class Stream
+  attr_accessor :name, :title, :game, :viewers, :game_changed, :still_live, :title_changed, :is_new_live, :should_delete
+
+  def initialize(n,t,g,v)
+    @name = n
+    @title = t
+    @game = g
+    @viewers = v
+    @still_live = true
+    @game_changed = false
+    @title_changed = false
+    @is_new_live = false
+    @should_delete = false
+  end
+
+  def to_string
+    "\x036\x1Fhttp://www.twitch.tv/#{@name}\x1F\x03 ::\x033 #{@title}\x03 ::\x036 Game:\x033 #{@game} \x036(#{@viewers} viewers)"
+  end
+end
+
+class NickServ
+  include Cinch::Plugin
+
+  listen_to :connect, method: :on_connect
+
+  def on_connect(m)
+    User("nickserv").send("identify tangoaway")
+  end
+end
+
+class Db
+  include Cinch::Plugin
+
+  listen_to :message, method: :on_message
+  listen_to :connect, method: :on_connect
+
+  def on_connect(m)
+    build_streams
+    t2 = Thread.new(update_streams)
+  end
+
+  def build_streams
+    $live_streams = []
+    new_streams = $db.execute("select * from stream where status = 1")
+    new_streams.each do |stream|
+      class_stream = Stream.new(stream[0],stream[1],stream[2],stream[3])
+      $live_streams.push(class_stream)
+    end
+  end
+
+  def update_streams
+    while true
+      begin
+        updated_streams = $db.execute("select * from stream where status = 1")
+        updated_class_streams = Hash.new "no stream"
+        updated_streams.each do |stream|
+          updated_class_streams[stream[0]] = Stream.new(stream[0],stream[1],stream[2],stream[3])
+        end
+
+        $live_streams.each do |live_stream|
+          live_stream.game_changed = false
+          live_stream.title_changed = false
+          live_stream.still_live = true
+          live_stream.is_new_live = false
+          live_stream.should_delete = false
+          if updated_class_streams.has_key?(live_stream.name)
+            class_stream = updated_class_streams[live_stream.name]
+            live_stream.still_live = true
+            live_stream.viewers = class_stream.viewers
+            if not class_stream.game == live_stream.game
+              live_stream.game_changed = true
+              live_stream.game = class_stream.game
+            end
+            if not class_stream.title == live_stream.title
+              live_stream.title_changed = true
+              live_stream.title = class_stream.title
+            end
+          else
+            live_stream.still_live = false
+          end
+        end
+
+        updated_class_streams.each do |key,value|
+          is_new_stream = true
+          $live_streams.each do |live_stream|
+            if value.name == live_stream.name
+              is_new_stream = false
+            end
+          end
+          if is_new_stream
+            value.is_new_live = true
+            $live_streams.push(value)
+          end
+        end
+
+        $live_streams.each do |live_stream|
+          if not live_stream.still_live
+            live_stream.should_delete = true
+          elsif live_stream.game_changed and not live_stream.title_changed
+            Channel("#tango").send("New Game - #{live_stream.to_string}")
+          elsif not live_stream.game_changed and live_stream.title_changed
+            Channel("#tango").send("New Title - #{live_stream.to_string}")
+          elsif live_stream.game_changed and live_stream.title_changed
+            Channel("#tango").send("New Title and Game - #{live_stream.to_string}")
+          elsif live_stream.is_new_live
+            Channel("#tango").send("Now Live - #{live_stream.to_string}")
+          end
+        end
+
+        $live_streams.delete_if { |x| x.should_delete}
+        $live_streams = $live_streams.sort! {|a,b| a.name <=> b.name}
+      rescue Exception => e
+      end
+      sleep(15)
+    end
+  end
+
+  def on_message(m)
+    msg = m.params[1]
+    if msg.index('.add') == 0
+      add_stream_to_db msg
+    elsif msg.index('.remove') == 0
+      remove_stream_from_db msg
+    elsif msg.index('.live') == 0
+      show_live_streams
+    elsif msg.index('.list') == 0
+      list_streams
+    elsif msg.index('.update') == 0
+      update_streams
+    end
+  end
+
+  def add_stream_to_db(msg)
+    begin
+      split_msg = msg.split(' ')
+      if split_msg.length > 2
+        return false
+      end
+      stream = split_msg[1]
+      if not is_in_db? stream
+        db_query = "insert into stream values (\'#{stream}\',\'\',\'\',0,0)"
+        $db.execute(db_query)
+        Channel("#tango").send("#{stream} successfully added")
+      else
+        Channel("#tango").send("#{stream} is already on the list!")
+      end
+    rescue
+      Channel("#tango").send("Error adding stream #{stream}. Try again foo.")
+    end
+  end
+
+  def remove_stream_from_db(msg)
+    begin
+      split_msg = msg.split(' ')
+      if split_msg.length > 2
+        return false
+      end
+      stream = split_msg[1]
+      if is_in_db? stream
+        db_query = "delete from stream where username = '#{stream}';"
+        $db.execute(db_query)
+        Channel("#tango").send("#{stream} successfully removed")
+      else
+        Channel("#tango").send("#{stream} is not in the list!")
+      end
+    rescue Exception => e
+      Channel("#tango").send("Error removing #{stream}")
+    end
+  end
+
+  def list_streams
+    begin
+      all_streams = $db.execute("select username from stream order by username")
+      Channel("#tango").send("Listing all streams for channel #tango")
+      msg = ""
+      all_streams.each do |stream|
+        msg = msg + stream[0] + ","
+      end
+      puts msg
+      msg = msg[0...-1]
+      Channel("#tango").send(msg)
+    rescue Exception => e
+      Channel("#tango").send("Error listing streams. Try again foo.")
+    end
+  end
+
+  def show_live_streams
+    if $live_streams.size > 0
+      $live_streams.each do |stream|
+        Channel("#tango").send("Currently live - #{stream.to_string}")
+      end
+    else
+      Channel("#tango").send("(◞‸◟)")
+    end
+  end
+
+  def is_in_db?(username)
+    begin
+      streams = $db.execute("select * from stream where username = '#{username}'")
+      return (streams.length > 0)
+    rescue Exception => e
+      Channel("#tango").send("Error (◞‸◟)")
+    end
+  end
+end
+
+bot = Cinch::Bot.new do
+  configure do |c|
+    c.server = "irc.speedrunslive.com"
+    c.channels = ["#tango"]
+    c.nicks = ["tango","tango2"]
+    c.realname = "tango"
+    c.user = "tango"
+    c.plugins.plugins = [NickServ,Db]
+  end
+
+  $db = SQLite3::Database.open 'streams.db'
+end
+
+bot.start
---
+++ twitch_api.rb
@@ -0,0 +1,28 @@
+require 'net/http'
+require 'multi_json'
+require 'sqlite3'
+
+$db = SQLite3::Database.open 'streams.db'
+
+while true do
+  begin
+    streams = $db.execute('select username from stream').join(',')
+    uri = URI("https://api.twitch.tv/kraken/streams?channel=#{streams}")
+    response = Net::HTTP.get_response(uri)
+    obj = MultiJson.load(response.body)
+
+    $db.execute("update stream set status = 0;")
+    if obj['_total'] > 0
+      obj['streams'].each do |k|
+        name = k['channel']['name']
+        game = k['channel']['game']
+        title = k['channel']['status']
+        viewers = k['viewers'].to_s
+        $db.execute("update stream set title = '#{title}', game = '#{game}', viewers = #{viewers}, status = 1 where username = '#{name}';")
+      end
+    end
+  rescue Exception => e
+    puts e
+  end
+  sleep(15)
+end
