6941992876afd7e7cef3daad53077785af8e9c6e
Blake <phillips.blakej@gmail.com>
2015-09-21 18:42:01 -0700

    added join/leave commands

--- bot/bot.rb
+++ bot/bot.rb
@@ -1,17 +1,18 @@
 require 'cinch'
 require 'sqlite3'
 require_relative 'db_accessor.rb'
-require_relative 'stream'
-require_relative 'nick_serv'
+require_relative 'stream.rb'
+require_relative 'nick_serv.rb'
+require_relative 'ctrl_commands.rb'
 
 bot = Cinch::Bot.new do
   configure do |c|
     c.server = "irc.speedrunslive.com"
-    c.channels = ["#tango"]
-    c.nicks = ["tango","tango2"]
+    c.channels = ["#tango", "#gdqsubmissions"]
+    c.nicks = ["tango","tango3469"]
     c.realname = "tango"
     c.user = "tango"
-    c.plugins.plugins = [NickServ,Db]
+    c.plugins.plugins = [NickServ,Db,CtrlCommands]
   end
 
   $db = SQLite3::Database.open 'data/streams.db'
---
+++ bot/ctrl_commands.rb
@@ -0,0 +1,20 @@
+class CtrlCommands
+  include Cinch::Plugin
+
+  listen_to :private, method: :on_private_message
+
+  def on_private_message(m)
+    if is_admin?(m)
+      msg = m.params[1]
+      if msg.index('join') == 0
+        Channel(msg.split(' ')[1]).join()
+      elsif msg.index('leave') == 0
+        Channel(msg.split(' ')[1]).part()
+      end
+    end
+  end
+
+  def is_admin?(m)
+    m.user.nick == 'boshi_ta'
+  end
+end
