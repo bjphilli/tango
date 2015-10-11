4628c656232e02f857d7d7662a6935dc832ff145
Blake <phillips.blakej@gmail.com>
2015-10-11 06:48:01 -0700

    fixed being-rescue for submission bot

--- submissions/submission_bot.rb
+++ submissions/submission_bot.rb
@@ -1,6 +1,13 @@
 require 'nokogiri'
 require 'open-uri'
 require 'pg'
+require 'time'
+
+def log_exception(e)
+  f = File.open('error_log.txt', 'a')
+  f.puts("[#{Time.now.utc.iso8601}]  - #{e}")
+  f.close()
+end
 
 $pg_sub = PGconn.open(:dbname => 'tango')
 
@@ -9,36 +16,40 @@ $pg_sub.prepare('update_submission', 'update submission set runner = $1,game = $
 $pg_sub.prepare('select_submission', 'select * from submission where runner = $1 and game = $2 and description = $3')
 
 while true
-  html_doc = Nokogiri::HTML(open('https://gamesdonequick.com/submission/all'))
-  table = html_doc.css('#datatable')
-  rows = table.css('tbody tr')
+  begin
+    html_doc = Nokogiri::HTML(open('https://gamesdonequick.com/submission/all'))
+    table = html_doc.css('#datatable')
+    rows = table.css('tbody tr')
 
-  rows.each do |row|
-    tds = row.css('td')
-    name = tds[0].text.strip
-    game = tds[1].text.strip
-    description = tds[2].text.strip
-    status1 = tds[3]["data-sort"]
-    category1 = tds[3].css('small strong').text.strip
-    status2 = tds[4]["data-sort"]
-    category2 = tds[4].css('small strong').text.strip
-    status3 = tds[5]["data-sort"]
-    category3 = tds[5].css('small strong').text.strip
-    status4 = tds[6]["data-sort"]
-    category4 = tds[6].css('small strong').text.strip
-    status5 = tds[7]["data-sort"]
-    category5 = tds[7].css('small strong').text.strip
+    rows.each do |row|
+      tds = row.css('td')
+      name = tds[0].text.strip
+      game = tds[1].text.strip
+      description = tds[2].text.strip
+      status1 = tds[3]["data-sort"]
+      category1 = tds[3].css('small strong').text.strip
+      status2 = tds[4]["data-sort"]
+      category2 = tds[4].css('small strong').text.strip
+      status3 = tds[5]["data-sort"]
+      category3 = tds[5].css('small strong').text.strip
+      status4 = tds[6]["data-sort"]
+      category4 = tds[6].css('small strong').text.strip
+      status5 = tds[7]["data-sort"]
+      category5 = tds[7].css('small strong').text.strip
 
-    result = $pg_sub.exec_prepared('select_submission', [name,game,description]).values
+      result = $pg_sub.exec_prepared('select_submission', [name,game,description]).values
 
-    if result.count == 0
-      puts "inserting new submission"
-      $pg_sub.exec_prepared('insert_submission', [name,game,description,category1,category2,category3,category4,category5,status1,status2,status3,status4,status5])
-    else
-      puts 'updating existing submission'
-      id = result[0][0]
-      $pg_sub.exec_prepared('update_submission', [name,game,description,category1,category2,category3,category4,category5,status1,status2,status3,status4,status5,id]) 
+      if result.count == 0
+        puts "inserting new submission"
+        $pg_sub.exec_prepared('insert_submission', [name,game,description,category1,category2,category3,category4,category5,status1,status2,status3,status4,status5])
+      else
+        puts 'updating existing submission'
+        id = result[0][0]
+        $pg_sub.exec_prepared('update_submission', [name,game,description,category1,category2,category3,category4,category5,status1,status2,status3,status4,status5,id]) 
+      end
     end
+  rescue Exception => e
+    log_exception(e)
   end
   sleep (120)
 end
